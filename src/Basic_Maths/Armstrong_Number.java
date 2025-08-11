88129f9f525a96b245f9a60bd666dbe775e20a12
H Jivan Singha <hjivansingha123@gmail.com>
2025-08-11 16:48:46 +0530

    Palindrome

--- src/Basic_Maths/Armstrong_Number.java
+++ src/Basic_Maths/Armstrong_Number.java
@@ -1,4 +1,27 @@
 package Basic_Maths;
 
 public class Armstrong_Number {
+    public static void main(String[] args) {
+        System.out.println(armstrong(1634));
+
+    }
+
+    public static boolean armstrong(int n){
+        int digit=0;
+        double sum=0;
+        int temp=n;
+        int temp2=n;
+        while(temp!=0){
+            temp=temp/10;
+            digit++;
+        }
+
+        while(n>0){
+            int ld=n%10;
+            sum=sum+Math.pow(ld,digit);
+            n=n/10;
+        }
+
+        return sum==temp2;
+    }
 }
--- src/Basic_Recursion/Check_if_a_string_is_Palindrome.java
+++ src/Basic_Recursion/Check_if_a_string_is_Palindrome.java
@@ -1,4 +1,32 @@
 package Basic_Recursion;
-
+import java.io.*;
+import java.util.Arrays;
 public class Check_if_a_string_is_Palindrome {
+    public static void main(String[] args) {
+        String s="jivanavij";
+        System.out.println(isPalindrome(s));
+
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left=0;
+        int right=  s.length()-1;
+        while(left<right){
+            //skiping non-alphanumeric character
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left ++;
+            right--;
+
+        }
+        return true;
+    }
 }
